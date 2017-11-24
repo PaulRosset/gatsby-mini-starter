820b46dec64e7f2dc89f639680fee2cfc70f381e
Paul Rosset <paulrosset96@gmail.com>
2017-11-24 10:08:34 +0000

    Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Minimal gatsby starter.`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`]
+};
